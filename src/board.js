a163c92e76d78228e84b73b95bfca81ecf68ea29
Sebastian Morales <sebastian.moralesd@gmail.com>
2017-06-11 17:56:15 -0700

    Move BoardHelper to its own file. Move game logic from Board to Game class

--- src/board.js
+++ src/board.js
@@ -3,30 +3,19 @@ import React from "react";
 import Square from "./square.js";
 
 export default class Board extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-    }
 
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const status = this.getStatusText();
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -45,59 +34,4 @@ export default class Board extends React.Component {
             </div>
         );
     }
-
-    handleClick(i) {
-        if (BoardHelper.calculateWinner(this.state.squares) || this.state.squares[i]) {
-            return;
-        }
-
-        const squares = this.state.squares.slice();
-        squares[i] = this.getNextPlayer();
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
-    getNextPlayer() {
-        if (this.state.xIsNext) {
-            return "X";
-        }
-
-        return "O";
-    }
-
-    getStatusText() {
-        const winner = BoardHelper.calculateWinner(this.state.squares);
-
-        if (winner) {
-            return "Winner: " + winner;
-        }
-
-        return "Next player: " + this.getNextPlayer();
-    }
-}
-
-class BoardHelper {
-    static calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-
-        return null;
-    }
-}
+ }
--- src/game.js
+++ src/game.js
@@ -1,19 +1,75 @@
 import React from "react";
 
 import Board from "./board.js";
+import BoardHelper from "./board-helper.js";
 
 export default class Game extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            xIsNext: true
+        };
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = BoardHelper.calculateWinner(current.squares);
+
+        let status = this.getStatusText(winner);
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
         );
+    }
+
+    getStatusText(winner) {
+        if (winner) {
+            return "Winner: " + winner;
+        }
+
+        return "Next player: " + this.getNextPlayer();
+    }
+
+    getNextPlayer() {
+        if (this.state.xIsNext) {
+            return "X";
+        }
+
+        return "O";
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (BoardHelper.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.getNextPlayer();
+
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            xIsNext: !this.state.xIsNext
+        });
     }
 }
